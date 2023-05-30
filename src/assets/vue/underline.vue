174506c61bef86dcaabdb2ed9bc8b7ac9e6c4fbe
liangjun <530080147@qq.com>
2023-05-30 16:14:57 +0800

    feat: 🎸 css underline

---
+++ src/assets/vue/underline.vue
@@ -0,0 +1,16 @@
+<template>
+  <span> Migrating From MySQL to YugabyteDB Using YugabyteDB Voyager</span>
+</template>
+
+<style scoped>
+span {
+  line-height: 1.5em;
+  background: linear-gradient(to right, #ec695c, #61c454) no-repeat right bottom;
+  background-size: 0 2px;
+  transition: background-size 1300ms;
+}
+span:hover {
+  background-position-x: left;
+  background-size: 100% 2px;
+}
+</style>
